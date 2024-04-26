c5ba6a645106aa02aa4dc6698b770831b92470ce
dem4ply <dem4ply@gmail.com>
2024-04-26 09:58:34 -0600

    implement basic logic to add

--- parking_lot/parking_lot.py
+++ parking_lot/parking_lot.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+import datetime
 
-app = Flask(__name__)
+       #{"car": "X774HY98", "tariff": "hourly", "location": 1, "start": "2014-10-01 14:11:45"},
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class Parking_lot:
+    loot = {}
+    amount_lot = 0
+    locations = []
+
+    def __init__( self, amount ):
+        self.amount_lot = amount
+        self.loot = {}
+        self.locations = []
+
+    def add( self, car, tariff ):
+        location = self.find_next_available_location()
+        start = datetime.datetime.now()
+        self.loot[ car ] = {
+            'car': car,
+            'tariff': tariff,
+            'location': location,
+            'start': start,
+        }
+
+    def find_next_available_location( self ):
+        return len( self.locations ) + 1
+
+    def __contains__( self, index ):
+        return index in self.loot
+
+    def __getitem__( self, index ):
+        return self.loot[ index ]
--- tests/test_parking_lot.py
+++ tests/test_parking_lot.py
@@ -1,16 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
+import datetime
 import unittest
+from parking_lot.parking_lot import Parking_lot
+from parking_lot.factories import car_add
 
+class Test_Parking_lot( unittest.TestCase ):
+    amount = 10
 
-class Test_Parking_lot(unittest.TestCase):
     def setUp(self):
-        pass
+        super().setUp()
+        self.parking_lot = Parking_lot( amount=self.amount )
 
-    def tearDown(self):
-        pass
+    def test_should_work( self ):
+        self.assertTrue( self.parking_lot )
 
-    def test_should_work(self):
-        pass
+
+class Test_Parking_lot_add( Test_Parking_lot ):
+
+    def setUp(self):
+        super().setUp()
+        self.new_car = car_add.build()
+
+    def test_add_car_should_be_in_the_parking( self ):
+        self.parking_lot.add( **self.new_car )
+        self.assertIn( self.new_car[ 'car' ], self.parking_lot )
+
+    def test_add_car_should_add_current_date_and_location( self ):
+        self.parking_lot.add( **self.new_car )
+        car_data = self.parking_lot[ self.new_car[ 'car' ] ]
+        self.assertIn( 'location', car_data )
+        self.assertIn( 'start', car_data )
+        self.assertTrue( car_data[ 'location' ] )
+        self.assertTrue( car_data[ 'start' ] )
+
+    def test_location_should_be_int( self ):
+        self.parking_lot.add( **self.new_car )
+        car_data = self.parking_lot[ self.new_car[ 'car' ] ]
+        self.assertIsInstance( car_data[ 'location' ], int )
+
+    def test_start_should_be_datetime( self ):
+        self.parking_lot.add( **self.new_car )
+        car_data = self.parking_lot[ self.new_car[ 'car' ] ]
+        self.assertIsInstance( car_data[ 'start' ], datetime.datetime )
+
+    def test_add_car_should_set_start_to_now_date( self ):
+        now = datetime.datetime.now()
+        self.parking_lot.add( **self.new_car )
+        car_data = self.parking_lot[ self.new_car[ 'car' ] ]
+        diff_date = car_data[ 'start' ] - now
+        self.assertLessEqual( abs( diff_date.total_seconds() ), 1 )
