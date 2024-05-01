93fad9d25d54d7e50a24ee21c6225aa7a968db20
dem4ply <dem4ply@gmail.com>
2024-04-30 18:01:40 -0600

    remove pep8 ignore rules

--- parking_lot/factories.py
+++ parking_lot/factories.py
@@ -3,68 +3,68 @@ from factory import fuzzy
 from parking_lot.parking_lot import Car as Car_model
 
 
-tariffs = [ 'hourly', 'daily' ]
+tariffs = ['hourly', 'daily']
 
 
-class Car( factory.Factory ):
-    car = factory.Faker( 'license_plate' )
-    tariff = fuzzy.FuzzyChoice( tariffs )
-    location = factory.Faker( 'pyint' )
-    hourly_fee = factory.Faker( 'pyint' )
-    daily_fee = factory.Faker( 'pyint' )
+class Car(factory.Factory):
+    car = factory.Faker('license_plate')
+    tariff = fuzzy.FuzzyChoice(tariffs)
+    location = factory.Faker('pyint')
+    hourly_fee = factory.Faker('pyint')
+    daily_fee = factory.Faker('pyint')
 
     class Meta:
         model = Car_model
 
 
-class Car_daily_fee( Car ):
+class Car_daily_fee(Car):
     tariff = 'hourly'
     start = factory.Faker(
-        'date_time_between', start_date='-15d', end_date='-1d' )
+        'date_time_between', start_date='-15d', end_date='-1d')
     finish = factory.Faker(
-        'date_time_between', start_date='now', end_date='now' )
+        'date_time_between', start_date='now', end_date='now')
 
 
-class Car_daily_free( Car_daily_fee ):
+class Car_daily_free(Car_daily_fee):
     start = factory.Faker(
-        'date_time_between', start_date='-15m', end_date='now' )
+        'date_time_between', start_date='-15m', end_date='now')
     finish = factory.Faker(
-        'date_time_between', start_date='now', end_date='now' )
+        'date_time_between', start_date='now', end_date='now')
 
 
-class Car_hourly_fee( Car ):
+class Car_hourly_fee(Car):
     tariff = 'hourly'
     start = factory.Faker(
-        'date_time_between', start_date='-15h', end_date='-1h' )
+        'date_time_between', start_date='-15h', end_date='-1h')
     finish = factory.Faker(
-        'date_time_between', start_date='now', end_date='now' )
+        'date_time_between', start_date='now', end_date='now')
 
 
-class Car_hourly_free( Car_hourly_fee ):
+class Car_hourly_free(Car_hourly_fee):
     start = factory.Faker(
-        'date_time_between', start_date='-15m', end_date='now' )
+        'date_time_between', start_date='-15m', end_date='now')
     finish = factory.Faker(
-        'date_time_between', start_date='now', end_date='now' )
+        'date_time_between', start_date='now', end_date='now')
 
 
-class Car_add( factory.Factory ):
-    car = factory.Faker( 'license_plate' )
-    tariff = fuzzy.FuzzyChoice( tariffs )
+class Car_add(factory.Factory):
+    car = factory.Faker('license_plate')
+    tariff = fuzzy.FuzzyChoice(tariffs)
 
     class Meta:
         model = dict
 
 
-class Car_add_hourly( factory.Factory ):
-    car = factory.Faker( 'license_plate' )
+class Car_add_hourly(factory.Factory):
+    car = factory.Faker('license_plate')
     tariff = 'hourly'
 
     class Meta:
         model = dict
 
 
-class Car_add_daily( factory.Factory ):
-    car = factory.Faker( 'license_plate' )
+class Car_add_daily(factory.Factory):
+    car = factory.Faker('license_plate')
     tariff = 'daily'
 
     class Meta:
--- parking_lot/parking_lot.py
+++ parking_lot/parking_lot.py
@@ -2,10 +2,10 @@
 import datetime
 
 
-class Parking_lot_error( Exception ):
+class Parking_lot_error(Exception):
     message = 'unhandle error with parking lot'
 
-    def __init__( self, message=None ):
+    def __init__(self, message=None):
         if message is None:
             message = self.message
         self.data = {
@@ -14,15 +14,15 @@ class Parking_lot_error( Exception ):
         }
 
 
-class Full_parking_error( Parking_lot_error ):
+class Full_parking_error(Parking_lot_error):
     message = 'No free space.'
 
 
-class Cannot_find_car_error( Parking_lot_error ):
+class Cannot_find_car_error(Parking_lot_error):
     message = 'Cannot find a car in the location {}.'
 
-    def __init__( self, location=None ):
-        message = self.message.format( location )
+    def __init__(self, location=None):
+        message = self.message.format(location)
         self.data = {
             'status': 'error',
             'error': message
@@ -32,7 +32,7 @@ class Cannot_find_car_error( Parking_lot_error ):
 class Car:
     def __init__(
             self, car, tariff, location, hourly_fee, daily_fee,
-            start=None, finish=None ):
+            start=None, finish=None):
         self.car = car
         self.tariff = tariff
         self.location = location
@@ -44,11 +44,11 @@ class Car:
         self.hourly_fee = hourly_fee
         self.daily_fee = daily_fee
 
-    def exit( self ):
+    def exit(self):
         self.finish = datetime.datetime.now()
 
     @property
-    def fee( self ):
+    def fee(self):
         if self.is_free_charge:
             return 0
 
@@ -57,35 +57,35 @@ class Car:
         if self.is_hourly:
             return self.proportional_diff_time * self.hourly_fee
         raise NotImplementedError(
-            f"the tariff {self.tariff} is not implemented" )
+            f"the tariff {self.tariff} is not implemented")
 
     @property
-    def is_free_charge( self ):
+    def is_free_charge(self):
         diff_time = self.finish - self.start
-        return diff_time.total_seconds() // ( 60 * 15 ) < 1
+        return diff_time.total_seconds() // (60 * 15) < 1
 
     @property
-    def is_daily( self ):
+    def is_daily(self):
         return self.tariff == 'daily'
 
     @property
-    def is_hourly( self ):
+    def is_hourly(self):
         return self.tariff == 'hourly'
 
     @property
-    def proportional_diff_time( self ):
+    def proportional_diff_time(self):
         diff_time = self.finish - self.start
         if self.is_hourly:
-            return ( diff_time.total_seconds() // 3600 ) + 1
+            return (diff_time.total_seconds() // 3600) + 1
         if self.is_daily:
-            return ( diff_time.total_seconds() // 86400 ) + 1
+            return (diff_time.total_seconds() // 86400) + 1
 
 
 class Parking_lot:
     locations = None
     lot = None
 
-    def __init__( self, amount, hourly_fee=1, daily_fee=20 ):
+    def __init__(self, amount, hourly_fee=1, daily_fee=20):
         self.lot = {}
         self.locations = []
         self.amount_lot = amount
@@ -93,58 +93,58 @@ class Parking_lot:
         self.daily_fee = daily_fee
 
     @property
-    def amount_lot( self ):
+    def amount_lot(self):
         return self._amount_lot
 
     @amount_lot.setter
-    def amount_lot( self, value ):
+    def amount_lot(self, value):
         self._amount_lot = value
-        self.locations = [ None ] * self._amount_lot
+        self.locations = [None] * self._amount_lot
         for v in self.lot.values():
-            self.locations[ v.location ] = v
+            self.locations[v.location] = v
 
-    def add( self, car, tariff ):
+    def add(self, car, tariff):
         if self.is_full:
             raise Full_parking_error
 
         location = self.find_next_available_location()
         result = Car(
             car=car, tariff=tariff, location=location,
-            hourly_fee=self.hourly_fee, daily_fee=self.daily_fee )
-        self.locations[ location ] = result
-        self.lot[ car ] = result
+            hourly_fee=self.hourly_fee, daily_fee=self.daily_fee)
+        self.locations[location] = result
+        self.lot[car] = result
         return result
 
-    def remove( self, location ):
-        car = self.locations[ location ]
+    def remove(self, location):
+        car = self.locations[location]
         if car is None:
-            raise Cannot_find_car_error( location )
-        self.locations[ location ] = None
-        del self.lot[ car.car ]
+            raise Cannot_find_car_error(location)
+        self.locations[location] = None
+        del self.lot[car.car]
         car.exit()
         return car
 
     @property
-    def is_full( self ):
+    def is_full(self):
         return (
-            sum( ( 1 for i in self.locations if i is not None ) )
-            == self.amount_lot )
-        return len( self.lot ) >= self.amount_lot
+            sum((1 for i in self.locations if i is not None))
+            == self.amount_lot)
+        return len(self.lot) >= self.amount_lot
 
     @property
-    def is_empty( self ):
+    def is_empty(self):
         return (
-            sum( ( 1 for i in self.locations if i is None ) )
-            == self.amount_lot )
+            sum((1 for i in self.locations if i is None))
+            == self.amount_lot)
 
-    def find_next_available_location( self ):
-        return self.locations.index( None )
+    def find_next_available_location(self):
+        return self.locations.index(None)
 
-    def __contains__( self, index ):
+    def __contains__(self, index):
         return index in self.lot
 
-    def __getitem__( self, index ):
-        return self.lot[ index ]
+    def __getitem__(self, index):
+        return self.lot[index]
 
-    def __iter__( self ):
-        return iter( self.lot.values() )
+    def __iter__(self):
+        return iter(self.lot.values())
